37da72a1a6e372cc0cb3327543bdde5c6ed80697
Ninh <nguyennniiiinh@gmail.com>
2019-03-27 12:31:49 +0700

    update index.js project

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-import Dog from "./Dog";
+var Dog = require("./Dog");
+var Cat = require("./Cat");
+var Mouse = require("./Mouse")
 
 var dog =  new Dog("Tommy");
+var cat = new Cat("Tom", "male");
+var mouse = new Mouse("Jerry", "female");
 
 dog.bark();
+cat.eat(mouse);
