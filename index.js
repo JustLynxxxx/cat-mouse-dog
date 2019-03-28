ada8514949d9e72ef229eb84a83927fdee2ee0de
Ninh <nguyennniiiinh@gmail.com>
2019-03-28 09:11:04 +0700

    add new cat named Evan

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import Mouse from "./Mouse";
 var dog =  new Dog("Tommy");
 var cat = new Cat("Tom", "male");
 var mouse = new Mouse("Jerry", "female");
+var evan = new Cat("Evan", "male")
 
 dog.bark();
 cat.eat(mouse);
