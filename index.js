6c84bd3f67b325d910947f49676606c95da14681
Ninh <nguyennniiiinh@gmail.com>
2019-03-28 09:40:58 +0700

    Add cat eat mouse named dr method

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ import Mouse from "./Mouse";
 var dog =  new Dog("Tommy");
 var cat = new Cat("Tom", "male");
 var mouse = new Mouse("Jerry", "female");
-var evan = new Cat("Evan", "male")
+var evan = new Cat("Evan", "male");
+var dr = new Mouse("Dr", "male");
 
 dog.bark();
 cat.eat(mouse);
 dog.eat(cat);
 dog.eat(evan);
+cat.eat(dr);
+
